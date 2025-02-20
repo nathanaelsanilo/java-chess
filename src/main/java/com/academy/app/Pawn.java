7ec5275840e4ee735f699bf468dea96c39da3bba
nathanaelsanilo <nathanaelsanilo@gmail.com>
2025-02-20 20:33:20 +0700

    init project

---
+++ src/main/java/com/academy/app/Pawn.java
@@ -0,0 +1,8 @@
+package com.academy.app;
+
+public class Pawn extends Piece {
+
+    public Pawn(int xPos, int yPos) {
+        super(xPos, yPos);
+    }
+}
---
+++ src/main/java/com/academy/app/Piece.java
@@ -0,0 +1,11 @@
+package com.academy.app;
+
+public class Piece {
+    public int xPos;
+    public int yPos;
+
+    public Piece(int xPos, int yPos) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+}
